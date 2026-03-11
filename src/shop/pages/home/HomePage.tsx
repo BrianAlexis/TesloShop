ec70d253b4cda70cd5d7a3a059a7b59ee0efd4c5
Brian Acuña <brian2012briga@gmail.com>
2026-03-10 23:46:28 -0300

    Adding filters, and UI changes

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,10 +1,17 @@
-import { Button } from "../../../components/ui/button"
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { products } from "@/mocks/products.mock"
+import CustomJumbotron from "@/shop/components/CustomJumbotron"
+import ProductsGrid from "@/shop/components/ProductsGrid"
+
 
 const HomePage = () => {
     return (
-        <div>
-            <Button className="font-primary">Hola</Button>
-        </div>
+        <>
+            <CustomJumbotron title="All products" subTitle="Ropa minimalista y elegante inspirada en el diseño futurista de Teslo. Calidad premium para un estilo atemporal." />
+
+            <ProductsGrid products={products} />
+            <CustomPagination totalPages={5} />
+        </>
     )
 }
 export default HomePage
